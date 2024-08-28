d0e000905cc38f4c6a67e2c87695c589d8dc08fe
Thad Hutcheson <thadhutch@gmail.com>
2024-08-28 00:51:16 -0500

    exporting the auth script as a function

--- authenticate.py
+++ authenticate.py
@@ -3,23 +3,22 @@ import time
 from selenium import webdriver  # Python Dynamic web scraping library
 
 
-url = 'https://premium.pff.com/nfl/teams/2022/REGPO/tampa-bay-buccaneers/schedule'
-signinurl = 'https://auth.pff.com'
-email = os.environ.get('PFF_EMAIL')
-pw = os.environ.get('PFF_PASSWORD')
-email_input = '//*[@id="login-form_email"]'
-pw_input = '//*[@id="login-form_password"]'
-login_submit = '//*[@id="sign-in"]'
-continuetostats = '/html/body/div/div/div/div/div/div/ul/li[2]/a'
+def login_to_pff():
+    signinurl = "https://auth.pff.com"
+    email = os.environ.get("PFF_EMAIL")
+    pw = os.environ.get("PFF_PASSWORD")
+    email_input = '//*[@id="login-form_email"]'
+    pw_input = '//*[@id="login-form_password"]'
+    login_submit = '//*[@id="sign-in"]'
 
-driver = webdriver.Chrome()
-driver.get(signinurl)
+    driver = webdriver.Chrome()
+    driver.get(signinurl)
 
+    driver.find_element("xpath", email_input).send_keys(email)
+    driver.find_element("xpath", pw_input).send_keys(pw)
 
-driver.find_element("xpath", email_input).send_keys(email)
-driver.find_element("xpath", pw_input).send_keys(pw)
+    time.sleep(3)
 
-time.sleep(3)
+    driver.find_element("xpath", login_submit).click()
 
-driver.find_element("xpath", login_submit).click()
-
+    return driver
